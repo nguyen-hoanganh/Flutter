58f6bd73c4fe968f14f3d32ffe57a999d3003f29
hoanganh <hoanganh.hust62@gmail.com>
2021-06-15 11:01:07 +0700

    first

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+String s = 'How are you';
+void main() => runApp(
+    Center(child: Text(
+      s,
+      textDirection: TextDirection.ltr
+    ),),
+  );
+
